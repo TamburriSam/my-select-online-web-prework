406918a28a9ee15a20d80b129fc5e973e78290c1
Sam Tamburri <tamburrisam@gmail.com>
2018-09-22 19:52:46 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,9 +2,11 @@ def my_select(collection)
   x = 0 
  select = []
  while x << array.length 
- select << yield(array[x])
- x += 1 
+ if << yield(array[x])
+ select << array[i]
  end
- return select
+ i+=1
+ end
+ select
  
 end
